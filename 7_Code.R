3c7f773d0e91371e53714ef0e4222cbe4cb716cc
Abby Vogel <vogel327@umn.edu>
2023-04-08 12:51:57 -0500

    build code for update; initialize quarto

--- 7_Code.R
+++ 7_Code.R
@@ -34,6 +34,25 @@ for(i in x){
   a <- a+1
 }
 
+time_delim <- unlist(time_delim)
+time_fread <- unlist(time_fread)
+
 library(gt)
 
+gt(data.frame("Time" = str_c(x, "patients", sep = " "),"Delim" = c(time_delim), "Fread" = c(time_fread)))
+
+
 library(googledrive)
+
+df <- makeSepsisDataset()
+
+# We have to write the file to disk first, then upload it
+df %>% write_csv("sepsis_data_temp.csv")
+
+# Uploading happens here
+sepsis_file <- drive_put(media = "sepsis_data_temp.csv", 
+                         path = "https://drive.google.com/drive/folders/1qbUCGdFgUDzipeupj2iNQsEPHRKm732W",
+                         name = "sepsis_data.csv")
+
+# Set the file permissions so anyone can download this file.
+sepsis_file %>% drive_share_anyone()
